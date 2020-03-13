044a06e62ca4ef88555821f0f91d7e0cf2120657
sanoopv3 <sanoopintegral@gmail.com>
2020-03-13 18:30:51 +0530

    Login page corrected

--- app/src/main/java/solarapp/android/integral/com/solarapp/WebAppInterface.java
+++ app/src/main/java/solarapp/android/integral/com/solarapp/WebAppInterface.java
@@ -1,6 +1,7 @@
 package solarapp.android.integral.com.solarapp;
 
 import android.content.Context;
+import android.os.Build;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
@@ -17,4 +18,20 @@ public class WebAppInterface {
 
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
+
+    @JavascriptInterface
+    public int getAndroidVersion() {
+
+        return Build.VERSION.SDK_INT;
+
+    }
+
+    @JavascriptInterface
+    public void showAndroidVersion(String versionName) {
+        Toast.makeText(context, versionName, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
 }
